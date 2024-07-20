8bf0363fc02e78da69fc58a12f770d44d65b509c
sniksanaik <sniksa10@gmail.com>
2024-07-20 09:03:29 +0530

    1sy pust of practice

---
+++ Try_practice/src/test/java/try_1/create_democlass.java
@@ -0,0 +1,11 @@
+package try_1;
+
+public class create_democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HI welcome to practice session");
+	}
+
+}
